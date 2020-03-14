28bfc2262983314d7cd32780d6a0f4131474dfd8
EC2 Default User <ec2-user@ip-172-31-16-65.us-east-2.compute.internal>
2020-03-14 12:03:08 +0000

    laravel13-1-1

---
+++ create.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.admin')
+
+@section('title', 'Myプロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                    
+                    @if (count($errors) > 0)
+                    <ul>
+                        @foreach($errors->all() as $e)
+                            <li>{{ $e }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名（name）</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                     <div class="form-group row">
+                        <label class="col-md-2">性別（gender）</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味（hobby）</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄（introduction）</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+@endsection
